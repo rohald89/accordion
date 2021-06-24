0363ebcad0f87b93ba38d5580d6ffc2fac8497e7
Rohald van Merode <rohald_89@live.nl>
2021-06-24 13:32:16 +0200

    added section for the questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import styled from 'styled-components';
+import {faq} from './data';
 
+const Container = styled.div`
+  display: flex;
+`;
 
 function App() {
+  console.log(faq);
   return (
     <div className="App">
-      test
+      <Container>
+          <img src="./images/illustration-woman-online-desktop.svg" alt="" />
+          <div className="faq-wrapper">
+            <h1>FAQ</h1>
+            <section className="questions">
+              {faq.map( (q, i) => {
+                return (<details className="faq" key={i}>
+                  <summary className="question">{q.question}</summary>
+                  <p>{q.answer}</p>
+                </details>)
+              })}
+              
+            </section>
+          </div>
+      </Container>
     
     </div>
   );
